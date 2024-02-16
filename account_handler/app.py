f3253aa9fbcd5a31de25b8e0661bc028eaa0fecd
caolan947 <caolan.daly94@gmail.com>
2024-02-16 14:47:20 +0000

    cosmetic changes

--- account_handler/app.py
+++ account_handler/app.py
@@ -4,22 +4,22 @@ import os
 
 # TODO unit test, integration test
 
-queue_url = os.environ['account_queue']
-msg_group = os.environ['queue_default_group']
+queue_url = os.environ["account_queue"]
+msg_group = os.environ["queue_default_group"]
 
-client = boto3.client('sqs')
+client = boto3.client("sqs")
 print(f"Created boto3 client for SQS")
 
 def lambda_handler(event, context):
     unpacked_event = UnpackEvent(event)
-    print(f"Formed message_body {unpacked_event.message_body} to send to queue {os.environ['account_queue']} in group {os.environ['queue_default_group']}")
+    print(f"Formed message_body {unpacked_event.message_body} to send to queue {queue_url} in group {msg_group}")
     
     res = sqs_send_message(unpacked_event.message_body)
 
-    if res['error']:
-        return Response(message=res['message'], status_code=500, data=res['data']).to_json()
+    if res["error"]:
+        return Response(message=res["message"], status_code=500, data=res["data"]).to_json()
 
-    return Response(message=res['message'], data=res['data']).to_json()
+    return Response(message=res["message"], data=res["data"]).to_json()
 
 def sqs_send_message(message_body):
     try:
@@ -50,11 +50,11 @@ def sqs_send_message(message_body):
 
 class UnpackEvent():
     def __init__(self, event):
-        self.event = json.loads(event['body'])
+        self.event = json.loads(event["body"])
         self.message_body = {
-            'action': 'register',
-            'username': event['username'],
-            'password': event['password']
+            "action": "register",
+            "username": self.event["username"],
+            "password": self.event["password"]
         }
         
         print(f"Unpacked event {event}")
