60e1827288dc728d5873f454f710a682c1f291e9
caolan947 <caolan.daly94@gmail.com>
2024-02-19 14:38:09 +0000

    define env var for region in unit tests to resolve breaking pipeline, some cosmetic tidy up

--- tests/unit/account_handler/test_app.py
+++ tests/unit/account_handler/test_app.py
@@ -4,8 +4,9 @@ import boto3
 from moto import mock_aws
 import os
 
-os.environ['account_queue'] = 'fake_queue.fifo'
-os.environ['queue_default_group'] = 'fake_group'
+os.environ["account_queue"] = "fake_queue.fifo"
+os.environ["queue_default_group"] = "fake_group"
+os.environ["region"] = "eu-west-1"
 
 from account_handler import app
 from botocore.exceptions import ClientError
@@ -15,22 +16,22 @@ class TestAccountHandlerApp(unittest.TestCase):
 
     @mock_aws
     def setUp(self):
-        self.fake_username = 'fake_username@email.com'
-        self.fake_password = 'fake_password'
+        self.fake_username = "fake_username@email.com"
+        self.fake_password = "fake_password"
         self.fake_event = {
             "body": f"{{\"username\": \"{self.fake_username}\", \"password\": \"{self.fake_password}\", \"action\": \"register\"}}"
         }
         self.loaded_body = {"username": self.fake_username, "password": self.fake_password}
 
-        self.fake_client = boto3.client('sqs', region_name="eu-west-1")
+        self.fake_client = boto3.client("sqs", region_name=os.environ["region"])
         self.fake_queue = self.fake_client.create_queue(
-            QueueName='fake_queue.fifo',
-            Attributes={'FifoQueue': 'true', 'ContentBasedDeduplication': 'true'
+            QueueName="fake_queue.fifo",
+            Attributes={"FifoQueue": "true", "ContentBasedDeduplication": "true"
         })
         self.fake_send_message_response = self.fake_client.send_message(
-            QueueUrl=self.fake_queue['QueueUrl'],
-            MessageBody=self.fake_event['body'],
-            MessageGroupId='fake_group'
+            QueueUrl=self.fake_queue["QueueUrl"],
+            MessageBody=self.fake_event["body"],
+            MessageGroupId="fake_group"
         )
 
         self.fake_context = LambdaContext()
@@ -38,17 +39,17 @@ class TestAccountHandlerApp(unittest.TestCase):
         self.fake_unpacked_event = Mock(
             event=self.loaded_body,
             message_body={
-                'action': 'register',
-                'username': self.fake_username,
-                'password': self.fake_password
+                "action": "register",
+                "username": self.fake_username,
+                "password": self.fake_password
             })
 
         self.fake_exception = ClientError(
-            operation_name='Test', 
+            operation_name="Test", 
             error_response={
-                'Error': {
-                    'Code': 'SomeError', 
-                    'Message': 'This is an error'
+                "Error": {
+                    "Code": "SomeError", 
+                    "Message": "This is an error"
                 }
             }
         )
@@ -65,20 +66,20 @@ class TestAccountHandlerApp(unittest.TestCase):
             "data": self.fake_exception
         } 
 
-    @patch('account_handler.app.Response')
-    @patch.object(app, 'sqs_send_message')
-    @patch('account_handler.app.UnpackEvent')
+    @patch("account_handler.app.Response")
+    @patch.object(app, "sqs_send_message")
+    @patch("account_handler.app.UnpackEvent")
     def test_lambda_handler(self, mock_unpacked_event, mock_res, mock_Response):
         mock_unpacked_event.return_value = self.fake_unpacked_event
         mock_res.return_value = self.fake_sqs_send_message_error_false
         mock_Response.return_value.to_response.return_value = {
-            'statusCode': 200,
-            'body': repr(self.fake_sqs_send_message_error_false)
+            "statusCode": 200,
+            "body": repr(self.fake_sqs_send_message_error_false)
         }
 
         expected_result = {
-            'statusCode': 200,
-            'body': repr(self.fake_sqs_send_message_error_false)
+            "statusCode": 200,
+            "body": repr(self.fake_sqs_send_message_error_false)
         }
         actual_result = app.lambda_handler(self.fake_event, self.fake_context)
         
@@ -87,20 +88,20 @@ class TestAccountHandlerApp(unittest.TestCase):
 
         mock_res.return_value = self.fake_sqs_send_message_error_true
         mock_Response.return_value.to_response.return_value = {
-            'statusCode': 500,
-            'body': repr(self.fake_sqs_send_message_error_false)
+            "statusCode": 500,
+            "body": repr(self.fake_sqs_send_message_error_false)
         }
 
         expected_result = {
-            'statusCode': 500,
-            'body': repr(self.fake_sqs_send_message_error_false)
+            "statusCode": 500,
+            "body": repr(self.fake_sqs_send_message_error_false)
         }
         actual_result = app.lambda_handler(self.fake_event, self.fake_context)
         
         with self.subTest():
             self.assertEqual(expected_result, actual_result)
 
-    @patch('account_handler.app.client')
+    @patch("account_handler.app.client")
     def test_sqs_send_message(self, mock_client):
         mock_client.send_message.return_value = self.fake_send_message_response
 
