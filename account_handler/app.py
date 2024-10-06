3c34e9f7a0f378014c23b218b029c9cb921b9383
caolan947 <caolan.daly94@gmail.com>
2024-10-06 13:49:37 +0100

    remove duplicate code, replace with lambdarepy lib, update requirements.txt files

--- account_handler/app.py
+++ account_handler/app.py
@@ -1,8 +1,7 @@
-import boto3
-import json
-import os
+import boto3, json, os
 from botocore.exceptions import ClientError
 import logging, logging.config
+from lambdarepy import EventFactory, Response
 
 logging.basicConfig(format='%(levelname)s %(message)s')
 logging.config.dictConfig({'version': 1, 'disable_existing_loggers': True})
@@ -13,9 +12,10 @@ logger.handlers[0].setFormatter(logging.Formatter(fmt='%(filename)s:%(lineno)s -
 queue_url = os.environ["account_queue"]
 msg_group = os.environ["queue_default_group"]
 client = boto3.client("sqs", region_name=os.environ["region"])
+print(f"Created SQS client in {os.environ["region"]}")
 
 def lambda_handler(event, context):
-    unpacked_event = UnpackEvent(event)
+    unpacked_event = EventFactory(event).get_event_parser()
     logger.info(f"Formed message_body {unpacked_event.message_body} to send to queue {queue_url} in group {msg_group}")
     
     res = sqs_send_message(unpacked_event.message_body)
@@ -50,42 +50,4 @@ def sqs_send_message(message_body):
             "error": True,
             "message": msg,
             "data": e
-        } 
-
-class UnpackEvent():
-    def __init__(self, event):
-        self.event = json.loads(event["body"])
-        self.message_body = {
-            "action": "register",
-            "username": self.event["username"],
-            "password": self.event["password"]
         }
-        
-        logger.info(f"Unpacked event {event}")
-
-class Response():
-    def __init__(self, message, status_code=200, error=False, data=None):
-        self.message = message
-        self.status_code = status_code
-        self.error = error
-        self.data=data
-        
-        if self.status_code != 200:
-            self.error = True
-            self.data = self.data
-
-        self.body_content = {
-            "error": self.error,
-            "message": message,
-            "data": self.data
-        }
-
-    def to_response(self):
-        self.response = {
-            "statusCode": self.status_code,
-            "body": repr(self.body_content)
-        }
-        
-        logger.info(f"Formed response {self.response}")
-
-        return self.response
