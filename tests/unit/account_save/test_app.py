ab7dd7ca864d8352fb46e59cc6b2b312c69d7d8c
caolan947 <caolan.daly94@gmail.com>
2024-10-07 10:23:35 +0100

    update existing tests to inherit reusable test data objects, clean up

--- tests/unit/account_save/test_app.py
+++ tests/unit/account_save/test_app.py
@@ -1,151 +1,71 @@
 import unittest
-from unittest.mock import patch, Mock
-import boto3, os
-from moto import mock_aws
-
+from unittest.mock import patch
+from .data import AccountSaveTestData
+import os
 os.environ["account_user_pool_client"] = "fake_id"
-os.environ["region"] = "eu-west-1"
-
 from account_save import app
-from botocore.exceptions import ClientError
-from aws_lambda_context import LambdaContext
 
 class TestAccountSaveApp(unittest.TestCase):
-
-    @mock_aws
     def setUp(self):
-        self.fake_username = "username"
-        self.fake_password = "Password123!"
-        self.fake_user_pool_name = "user_pool_name"
-        self.fake_client_name = "client_name"
-        self.fake_event = {
-            "body": f"{{\"username\": \"{self.fake_username}\", \"password\": \"{self.fake_password}\"}}"
-        }
-        self.fake_context = LambdaContext()
-
-        self.loaded_body = {"username": self.fake_username, "password": self.fake_password}
-
-        self.fake_client = boto3.client("cognito-idp", region_name=os.environ["region"])
-
-        self.fake_user_pool_response = self.fake_client.create_user_pool(
-            PoolName=self.fake_user_pool_name
-        )
-
-        self.fake_user_pool_client_response = self.fake_client.create_user_pool_client(
-            UserPoolId=self.fake_user_pool_response["UserPool"]["Id"],
-            ClientName=self.fake_client_name
-        )
-        
-        self.fake_client_id = self.fake_user_pool_client_response["UserPoolClient"]["ClientId"]
-
-        self.fake_sign_up_response = self.fake_client.sign_up(
-            ClientId=self.fake_client_id,
-            Username=self.fake_username,
-            Password=self.fake_password
-        )
-
-        self.fake_unpacked_message = Mock(
-            event=self.loaded_body,
-            message_body={
-                "username": self.fake_username,
-                "password": self.fake_password
-            })
-        
-        self.fake_exception = ClientError(
-            operation_name="Test", 
-            error_response={
-                "Error": {
-                    "Code": "SomeError", 
-                    "Message": "This is an error"
-                }
-            }
-        )
-
-        self.fake_user_exists_exception = ClientError(
-            operation_name="SignUp", 
-            error_response={
-                "Error": {
-                    "Code": "UsernameExistsException", 
-                    "Message": "An account with the given email already exists."
-                }
-            }
-        )
-
-        self.fake_cognito_sign_up_error_false = {
-            "error": False,
-            "message": f"Successfully signed up new user {self.fake_username} in Cognito user pool {self.fake_client_id} and got response {self.fake_sign_up_response}",
-            "data": self.fake_sign_up_response
-        }
-
-        self.fake_cognito_sign_up_error_true_generic = {
-            "error": True,
-            "message": f"An unexpected error occurred when creating user {self.fake_username} in Cognito user pool {self.fake_client_id} and raised exception {repr(self.fake_exception)}",
-            "data": self.fake_exception
-        }
-
-        self.fake_cognito_sign_up_error_true_user_exists = {
-            "error": True,
-            "message": f"An account with the given email already exists.",
-            "data": self.fake_user_exists_exception
-        }
+        self.test_data = AccountSaveTestData()
 
     @patch("account_save.app.Response")
     @patch.object(app, "cognito_sign_up")
     @patch("account_save.app.EventFactory")
     def test_lambda_handler(self, mock_unpacked_unpacked_message, mock_res, mock_Response):
-        mock_unpacked_unpacked_message.return_value.get_event_parser.return_value = self.fake_unpacked_message
-        mock_res.return_value = self.fake_cognito_sign_up_error_false
+        mock_unpacked_unpacked_message.return_value.get_event_parser.return_value = self.test_data.fake_unpacked_message
+        mock_res.return_value = self.test_data.fake_cognito_sign_up_error_false
         mock_Response.return_value.to_json.return_value = {
             "statusCode": 200,
-            "body": self.fake_cognito_sign_up_error_false
+            "body": self.test_data.fake_cognito_sign_up_error_false
         } 
 
         expected_result = {
             "statusCode": 200,
-            "body": self.fake_cognito_sign_up_error_false
+            "body": self.test_data.fake_cognito_sign_up_error_false
         }
-        actual_result = app.lambda_handler(self.fake_event, self.fake_context)
+        actual_result = app.lambda_handler(self.test_data.fake_event, self.test_data.fake_context)
         
         with self.subTest():
             self.assertEqual(expected_result, actual_result)
 
-        mock_res.return_value = self.fake_cognito_sign_up_error_true_user_exists
+        mock_res.return_value = self.test_data.fake_cognito_sign_up_error_true_user_exists
         mock_Response.return_value.to_json.return_value = {
             "statusCode": 500,
-            "body": self.fake_cognito_sign_up_error_true_user_exists
+            "body": self.test_data.fake_cognito_sign_up_error_true_user_exists
         }
 
         expected_result = {
             "statusCode": 500,
-            "body": self.fake_cognito_sign_up_error_true_user_exists
+            "body": self.test_data.fake_cognito_sign_up_error_true_user_exists
         }
-        actual_result = app.lambda_handler(self.fake_event, self.fake_context)
+        actual_result = app.lambda_handler(self.test_data.fake_event, self.test_data.fake_context)
         
         with self.subTest():
             self.assertEqual(expected_result, actual_result)
     
     @patch("account_save.app.client")
     def test_cognito_sign_up(self, mock_client):
-        mock_client.sign_up.return_value = self.fake_sign_up_response
+        mock_client.sign_up.return_value = self.test_data.fake_sign_up_response
 
-        expected_result = self.fake_cognito_sign_up_error_false
-        actual_result = app.cognito_sign_up(self.fake_client_id, self.loaded_body["username"], self.loaded_body["password"])
-
-        with self.subTest():
-            self.assertEqual(expected_result, actual_result)
-
-        mock_client.sign_up.side_effect = self.fake_exception
-
-        expected_result = self.fake_cognito_sign_up_error_true_generic
-        actual_result = app.cognito_sign_up(self.fake_client_id, self.loaded_body["username"], self.loaded_body["password"])
+        expected_result = self.test_data.fake_cognito_sign_up_error_false
+        actual_result = app.cognito_sign_up(self.test_data.fake_client_id, self.test_data.user_pw_body["username"], self.test_data.user_pw_body["password"])
 
         with self.subTest():
             self.assertEqual(expected_result, actual_result)
 
-        mock_client.sign_up.side_effect = self.fake_user_exists_exception
+        mock_client.sign_up.side_effect = self.test_data.fake_exception
 
-        expected_result = self.fake_cognito_sign_up_error_true_user_exists
-        actual_result = app.cognito_sign_up(self.fake_client_id, self.loaded_body["username"], self.loaded_body["password"])
+        expected_result = self.test_data.fake_cognito_sign_up_error_true_generic
+        actual_result = app.cognito_sign_up(self.test_data.fake_client_id, self.test_data.user_pw_body["username"], self.test_data.user_pw_body["password"])
+
+        with self.subTest():
+            self.assertEqual(expected_result, actual_result)
+
+        mock_client.sign_up.side_effect = self.test_data.fake_user_exists_exception
+
+        expected_result = self.test_data.fake_cognito_sign_up_error_true_user_exists
+        actual_result = app.cognito_sign_up(self.test_data.fake_client_id, self.test_data.user_pw_body["username"], self.test_data.user_pw_body["password"])
 
         with self.subTest():
             self.assertEqual(expected_result, actual_result)
