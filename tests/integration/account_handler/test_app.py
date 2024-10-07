d2cb7f7450d3ea0e08c5928e50fb93e86f3c94db
caolan947 <caolan.daly94@gmail.com>
2024-10-07 11:50:50 +0100

    update integration tests with reusable test fixtures, update dependencies, tidy up code and imports

--- tests/integration/account_handler/test_app.py
+++ tests/integration/account_handler/test_app.py
@@ -1,29 +1,38 @@
 from unittest import TestCase
-import os
-import requests
-import boto3
+import os, requests, boto3
 from botocore.exceptions import ClientError
+from ...data import TestData
 
 class TestAccountHandler(TestCase):
 
     def setUp(self):
-        self.api_endpoint = os.environ['API_ENDPOINT']
-        self.username = "test1@email.com"
-
-        self.event = {"username": self.username, "password": "password1"}
+        self.test_data = TestData()
 
     def test_account_handler(self):
         headers = headers = {'Content-type': 'application/json', 'Accept':'application/json'}
         
-        actual_result = requests.post(self.api_endpoint, json=self.event, headers=headers)
+        actual_result = requests.post(
+            os.environ['API_ENDPOINT'],
+            json=self.test_data.user_pw_body,
+            headers=headers
+        )
 
         with self.subTest():
             self.assertTrue(actual_result.ok)
         
     def tearDown(self):
-        client = boto3.client("cognito-idp",region_name="eu-west-1",aws_access_key_id=os.environ['PIPELINE_TESTER_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['PIPELINE_TESTER_SECRET_ACCESS_KEY'])
+        client = boto3.client(
+            "cognito-idp",
+            region_name=os.environ["region"],
+            aws_access_key_id=os.environ['PIPELINE_TESTER_ACCESS_KEY_ID'],
+            aws_secret_access_key=os.environ['PIPELINE_TESTER_SECRET_ACCESS_KEY']
+        )
         
         try:
-            client.admin_delete_user(UserPoolId="eu-west-1_jGwoeJ8Q0", Username=self.username)
+            client.admin_delete_user(
+                UserPoolId=f"{os.environ['region']}_jGwoeJ8Q0",
+                Username=self.test_data.fake_username
+            )
+
         except ClientError as e:
             pass
--- tests/unit/account_handler/data.py
+++ tests/unit/account_handler/data.py
@@ -2,7 +2,7 @@ import boto3, os
 from moto import mock_aws
 from unittest.mock import Mock
 
-from tests.test_data import TestData
+from logbook.tests.data import TestData
 
 class AccountHandlerTestData(TestData):
 
